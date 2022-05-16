7b8316fc751970d4354a6a11337b3025ea68197a
Aman Asthana <aman_asthana@thbs.india.com>
2022-05-16 19:10:57 +0530

    Dynamic Listing started

--- src/app/washingmachine/washingmachine.component.ts
+++ src/app/washingmachine/washingmachine.component.ts
@@ -9,11 +9,103 @@ import { DataApiService } from "../data-api.service";
 export class WashingmachineComponent implements OnInit {
 
   washingMachines=[];
+  products = [];
   constructor(private dataApi: DataApiService) { }
 
-  ngOnInit() {
-    this.washingMachines = this.dataApi.getAllWashingMachines();
-    console.log(this.dataApi.getWashingMachineById(1));
+  ngOnInit() {console.log('ngOnInit');
+  let param: any ='Washing Machine';
+  const URL = 'api/productBycategoryName'
+  console.log(URL);
+  const postData=
+  {
+    'categoryName':param
+  };
+  console.log(postData)
+  let responsePromise=my_async_fn(URL,'Washing Machine');
+  console.log("response : "+responsePromise);
+  responsePromise.then(resObj => {
+    console.log("resObj "+resObj)
+    let productsArr=resObj.product;
+    productsArr.forEach(productObj => {
+    console.log('prod name : ', productObj.name)
+    console.log('prod name : ',productObj.description)
+    let was=
+    {
+      "pic":productObj.pic,
+      "name":productObj.name,
+      "color":productObj.color,
+      "price":productObj.price,
+      "desc":productObj.description
+    }
+    console.log('was obj : ',was);
+    this.washingMachines.push(was);
+    })
+  })
+  console.log('washingMachines list : '+this.washingMachines);
+  // getWashingMachines();
+ }
+}
+async function my_async_fn(url,param) {
+  try{
+    const postData=
+    {
+      'categoryName':param
+    };
+    const settings = {
+      method: 'POST',
+      headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postData)
+  };
+  let response = await fetch(url, settings);
+  console.log('my_async_fn '+response); // Logs the response
+  if (response.ok) 
+  {
+    //return json
+    let data=response.json();
+    console.log('my_async_fn if condition'+data);
+    return data
+} else {
+    return null;
+}
+}
+  catch(e){
+    return e;
+  }
   }
 
-}
+  function getWashingMachines()
+  {
+    console.log('getWashingMachines');
+    let param: any ='Washing Machine';
+    const URL = 'api/productBycategoryName'
+    console.log(URL);
+    const postData=
+    {
+      'categoryName':param
+    };
+    console.log(postData)
+    let responsePromise=my_async_fn(URL,'Washing Machine');
+    console.log("response : "+responsePromise);
+    responsePromise.then(resObj => {
+      console.log("resObj "+resObj)
+      let productsArr=resObj.product;
+      productsArr.forEach(productObj => {
+      console.log('prod name : ', productObj.name)
+      console.log('prod name : ',productObj.description)
+      let was=
+      {
+        "pic":productObj.pic,
+        "name":productObj.name,
+        "color":productObj.color,
+        "price":productObj.price,
+        "desc":productObj.description
+      }
+      console.log('was obj : ',was);
+      this.mobiles.push(was);
+      })
+    })
+    console.log('washingMachines list : '+this.washingMachines);
+  }
